86976aee4cb4087e5485df226fd47d67e39c21d2
Raunak Rai <raunak96963@gmail.com>
2026-05-24 10:58:55 +0530

    Update next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,18 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: '*.supabase.co',
-        pathname: '/**',
-      },
-    ],
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
 
